f4718da5132b83668fc5f33dd4184c5095cd644d
Yovel <yovel.trichy@gmail.com>
2024-04-24 07:03:44 +0530

    Initial commit

---
+++ src/test/java/com/projects/productms/ProductMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.productms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
